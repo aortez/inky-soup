1b2e51ac0ccb513f8f7e1f85e26b4aa5d6ead886
Allan Ortiz <aortez@gmail.com>
2021-08-14 21:26:15 -0700

    Streamlined workflow to a single page.
    Previously we were going to a success page after submission. Now
    we just have an optional area at the top of the page where we display
    success or error messages from the previous time the user submitted
    the form.

--- upload-server/src/main.rs
+++ upload-server/src/main.rs
@@ -8,9 +8,13 @@ use rocket::fs::{FileServer, TempFile};
 use rocket::http::{ContentType, Status};
 use rocket::serde::Serialize;
 
-use std::env;
 use std::fs;
 use std::process::Command;
+
+#[derive(Debug, FromForm)]
+struct DeleteSubmission {
+    image_file_path: String
+}
 
 #[derive(Debug, FromForm)]
 struct FlashSubmission {
@@ -26,17 +30,12 @@ struct UploadSubmission<'v> {
     // TODO: validator for any image type that seems to work with the
     // flashing script.
     // #[field(validate = ext(ContentType::PNG))]
-    #[field(validate = ext(ContentType::PNG))]
     file: TempFile<'v>,
-
-    saturation: f32,
-
-    flash_twice: bool,
 }
 
 #[derive(Debug, FromForm)]
-struct SubmitNewImage<'v> {
-    submission: UploadSubmission<'v>,
+struct SubmitDeleteImage {
+    submission: DeleteSubmission,
 }
 
 #[derive(Debug, FromForm)]
@@ -44,13 +43,17 @@ struct SubmitFlashImage {
     submission: FlashSubmission,
 }
 
+#[derive(Debug, FromForm)]
+struct SubmitNewImage<'v> {
+    submission: UploadSubmission<'v>,
+}
+
 #[derive(Serialize)]
 #[serde(crate = "rocket::serde")]
-struct TemplateContext<'r> {
-    title: &'r str,
+struct TemplateContext {
     images: Vec<String>,
-    values: Vec<&'r str>,
-    errors: Vec<&'r str>
+    values: Vec<String>,
+    errors: Vec<String>
 }
 
 fn get_gallery_image_paths() -> Vec<String> {
@@ -74,11 +77,51 @@ fn upload_form() -> Template {
     println!("populating list of images in gallery...");
 
     Template::render("index", &TemplateContext {
-        title: "hideho",
         images: get_gallery_image_paths(),
-        values: vec!["One", "Two", "Three"],
+        values: vec!["Welcome!".to_string()],
         errors: vec![],
     })
+}
+
+#[post("/delete", data = "<form>")]
+async fn submit_delete_image<'r>(mut form: Form<Contextual<'r, SubmitDeleteImage>>) -> (Status, Template) {
+
+    let mut errors: Vec<String> = Vec::new();
+    let mut messages: Vec<String> = Vec::new();
+
+    match form.value {
+        Some(ref mut submission) => {
+            println!("submission: {:#?}", submission);
+
+            let image_file = format!("static/{}", submission.submission.image_file_path.clone());
+            println!("image_file: {}", image_file);
+
+            // Delete it!
+            let remove_result = fs::remove_file(image_file.clone());
+            println!("remove_result: {:#?}", remove_result);
+
+            match remove_result {
+                Ok(_e) => {
+                    messages.push(format!("succesfully removed image: {}", submission.submission.image_file_path.clone()));
+                },
+                Err(e) => {
+                    println!("{:#?}", e);
+                    errors.push(format!("A very sad error: {}", e));
+                }
+            }
+        }
+        None => {
+            errors.push("it is broke".to_string());
+        }
+    };
+
+    let template = Template::render("index", &TemplateContext {
+            images: get_gallery_image_paths(),
+            values: messages,
+            errors: errors,
+    });
+
+    (form.context.status(), template)
 }
 
 #[post("/flash", data = "<form>")]
@@ -132,7 +175,11 @@ async fn submit_new_image<'r>(mut form: Form<Contextual<'r, SubmitNewImage<'r>>>
             let gallery_result = file.copy_to(image_file_path.clone()).await;
             println!("image_file_path: {}, gallery_result: {:#?}", image_file_path, gallery_result);
 
-            Template::render("success", &form.context)
+            Template::render("index", &TemplateContext {
+                images: get_gallery_image_paths(),
+                values: vec![format!("Upload successful! Uploaded: {:#?}", file.raw_name().unwrap().dangerous_unsafe_unsanitized_raw())],
+                errors: vec![],
+            })
         }
         None => Template::render("index", &form.context),
     };
@@ -146,7 +193,7 @@ fn rocket() -> _ {
     println!("created images directory: {:#?}", create_result);
 
     rocket::build()
-        .mount("/", routes![submit_flash_image, submit_new_image, upload_form])
+        .mount("/", routes![submit_delete_image, submit_flash_image, submit_new_image, upload_form])
         .mount("/", FileServer::from("static"))
         .attach(Template::fairing())
 }
